155938e0cfe48cf4a667f2019d0096e07c711074
Eвгений <mocard778@gmail.com>
2023-02-03 15:41:30 +0700

    1 Задача

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,17 @@ public class Main {
     }
     public static void task1(){
         System.out.println("Задача 1");
+        int clientOS = 1;
+        switch (clientOS){
+            case 0:{
+                System.out.println("Установите версию приложения для iOS по ссылке");}
+            break;
+            case 1:{
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+            }
 
+
+        }
     }
 }
